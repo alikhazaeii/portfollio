cd41533a242cea022ea214be046e85acd22b20e0
Ali <ali.tradding021@gmail.com>
2024-12-08 23:05:08 +0330

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Spotlight } from '@/components/ui/Spotlight'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main className='flex relative justify-center items-center'>
+      <div className='w-full h-screen bg-black relative'>
+        <Spotlight className='left-96 ' fill='white'/>
+        <Spotlight className='left-52'fill='purple'/>
+        <Spotlight/>
+      </div>
+    </main>
+    
   )
 }
