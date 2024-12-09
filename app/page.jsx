e77c3f8fbac2ee19791b540e2acc66e2d285ba02
Ali <ali.tradding021@gmail.com>
2024-12-09 20:21:57 +0330

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,14 @@ import { Spotlight } from '@/components/ui/Spotlight'
 import { FloatingNav } from '@/components/ui/floating-navbar'
 
 
+
 export default function Home() {
   return (
     <main className='flex relative justify-center items-center overflow-hidden mx-auto sm:px-10 px-5 bg-[#000319]'>
       <FloatingNav/>
       <div className='w-full max-w-7xl'>
       <Hero/>
+      
       </div>
     </main>
     
