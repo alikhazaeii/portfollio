a02f4c32b105dda30bd093ee9aacada119d2a1fe
Ali <ali.tradding021@gmail.com>
2024-12-09 19:22:30 +0330

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
 'use client'
-import Hero from '@/components/Hero'
-import { FloatingNav } from '@/components/ui/floating-navbar'
-import { Spotlight } from '@/components/ui/Spotlight'
 import React from 'react'
+import Hero from '@/components/Hero'
+
+import { Spotlight } from '@/components/ui/Spotlight'
+import { FloatingNav } from '@/components/ui/floating-navbar'
+
 
 export default function Home() {
   return (
     <main className='flex relative justify-center items-center overflow-hidden mx-auto sm:px-10 px-5 bg-[#000319]'>
-      <div className='w-full max-w-7xl'>
       <FloatingNav/>
+      <div className='w-full max-w-7xl'>
       <Hero/>
       </div>
     </main>
