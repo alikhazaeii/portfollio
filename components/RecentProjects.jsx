69984dcb2475f06ff3050ecfab5b83ca1f907b8b
Ali <ali.tradding021@gmail.com>
2024-12-10 09:53:08 +0330

    commit

--- components/RecentProjects.jsx
+++ components/RecentProjects.jsx
@@ -10,10 +10,25 @@ export default function Projects() {
         <span className='text-orange-400 text-6xl'>recent projects</span>
       </h2>
       <div className='flex flex-wrap items-center justify-center p-4 gap-16  mt-10'>
-        {MyProjects.map(({ id, tilte, des, img, link }) => (
-          <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
-            <PinContainer>
-              {tilte}
+        {MyProjects.map(({ id, tilte, des, img, link ,iconsLists }) => (
+          <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[90vw]'>
+            <PinContainer title={tilte} href={link}>
+              <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                <figure className='relative w-full h-full overflow-hidden lg:rounded-3xl '>
+                  <img className='absolute z-10 bottom-0' src={img} alt="" />
+                  </figure>      
+              </div>
+              <h2 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1 text-orange-400'>
+                {tilte}
+              </h2>
+              <p className='lg:text-xl lg:font-normal font-light line-clamp-2 '>
+                {des}
+              </p>
+              <div className='flex justify-center items-center mt-7 mb-3'>
+                <div className='flex items-center'>
+                  {iconsLists}
+                </div>
+              </div>
             </PinContainer>
           </div>
         ))}
--- data/Myprojects.jsx
+++ data/Myprojects.jsx
@@ -1,36 +1,45 @@
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+
+
+
 export const MyProjects =[{
   id:'1',
   tilte : 'todoList',
   des:'The language Tools Works in React , Next',
-  img:'',
-  link:''
+  img:'todolist.png',
+  link:'https://todolist-react-2mmh.vercel.app/',
+  iconsLists:( <> 
+  <FaReact/> <RiNextjsFill/>
+  </>)
 },
 {
   id:'2',
   tilte : 'weather api',
   des:'The language Tools Works in React , Next , Api',
-  img:'',
-  link:''
+  img:'weather.png',
+  link:'https://alikhazaeii.github.io/weatherApi/'
 },
 {
   id:'3',
   tilte : 'mui Dashboard',
   des:'The language Tools Works in Next , mui',
-  img:'',
-  link:''
+  img:'mui.png',
+  link:'https://mui-admin-panel-ows2.vercel.app/'
 },
 {
   id:'4',
   tilte : 'AdminPanel',
   des:'The language Tools Works in React',
-  img:'',
-  link:''
+  img:'musicPleyer.png',
+  link:'https://alikhazaeii.github.io/radiojavan-musicpleyer/'
+
 },
 {
   id:'5',
-  tilte : 'shop Product',
+  tilte : 'curyptocurrency',
   des:'The language Tools Works in React , Next',
-  img:'',
-  link:''
+  img:'curyptocurrency.png',
+  link:'https://alikhazaeii.github.io/Cryptocurrency-FreeApi/'
 },
 ]
