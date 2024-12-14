25f928eb900fa03ef7f67c0dfbdb7e8bfc425e47
Ali <ali.tradding021@gmail.com>
2024-12-14 14:02:50 +0330

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children,
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased cursor-none`}
 
